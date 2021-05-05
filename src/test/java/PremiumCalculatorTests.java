2ef201b502a5f63c295d37e8561141f94cdcb125
Dmitrijs Kozlovskis <dmitrij.kozlovskij@gmail.com>
2021-05-05 18:10:33 +0300

    First criteria test added

--- src/test/java/PremiumCalculatorTests.java
+++ src/test/java/PremiumCalculatorTests.java
@@ -2,15 +2,21 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PremiumCalculatorTests {
+
     @Test
-    public void dummyTest() {
+    public void policy_with_one_object_and_two_sub_objects_criteria() {
         PremiumCalculator calculator = new DefaultPremiumCalculator();
+        PolicyObject policyObject = new PolicyObject("House");
+        policyObject.addItem(new PolicyObjectItem("TV", new BigDecimal("100.00"), RiskType.FIRE));
+        policyObject.addItem(new PolicyObjectItem("Refrigerator", new BigDecimal("8.00"), RiskType.THEFT));
+        Policy policy = new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED);
+        policy.addObject(policyObject);
 
-        BigDecimal result = calculator.calculate(new Policy());
+        BigDecimal calculatedPremium = calculator.calculate(policy);
 
-        assertNull(result);
+        assertEquals(0, calculatedPremium.compareTo(new BigDecimal("2.28")));
     }
 }
